48e9ca1f0440b4f5589791e959bdfdb218a8d709
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-02-16 19:12:09 +0600

    frontend home, product screen

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,81 @@
+import PropType from "prop-types";
+
+const Rating = ({ rating, text, color }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            rating >= 1
+              ? "fas fa-star"
+              : rating >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            rating >= 2
+              ? "fas fa-star"
+              : rating >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            rating >= 3
+              ? "fas fa-star"
+              : rating >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            rating >= 4
+              ? "fas fa-star"
+              : rating >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: color }}
+          className={
+            rating >= 5
+              ? "fas fa-star"
+              : rating >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+
+Rating.propTypes = {
+  rating: PropType.number.isRequired,
+  text: PropType.string.isRequired,
+  color: PropType.string,
+};
+
+export default Rating;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,65 @@
+import { Button, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductScreen = () => {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt="" fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price</Col>
+                <Col>{product.price}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status</Col>
+                <Col>
+                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className="btn btn-block"
+                type="button"
+                disabled={product.countInStock === 0}
+              >
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
